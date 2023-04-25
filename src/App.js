58bc3031dda92dc02e48f768198bb91b41e564b6
Roshdi Raed <roshdi013@gmail.com>
2023-04-25 15:41:10 +0200

    weather-react-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import WeatherApp from './components/WeatherApp';
+import PDFM from '../src/components/pdf';
 function App() {
   return (
     <>
       <WeatherApp />
+      <PDFM />
     </>
   );
 }
---
+++ src/components/pdf.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./WeatherApp.css";
+const PDFM = () => {
+    return (
+        <div>
+            <h1>Download PDF</h1>
+            <a href="https://download1075.mediafire.com/urif6a8wwujg6Svf4KOj_dZg0vdElTFBl2xRV5mBicZxrsH1PMrWev4l_Kj8WAtcHFHKOpHBpkWxHP5vy1ezsUAuXxo/mmt7vc9p0rb123i/How+to+Not+Die+Alone.pdf" ><button className='pdf'>Download</button></a>
+        </div>
+    )
+}
+
+export default PDFM
